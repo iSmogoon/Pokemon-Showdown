ab05499d8cba74fc8c03f4d312dd0aef2141b4e2
King Smogoon <iSmogoon2@gmail.com>
2015-08-04 19:22:40 -0600

    Update moves.js

--- mods/dgm/moves.js
+++ mods/dgm/moves.js
@@ -303,6 +303,28 @@ exports.BattleMovedex = {
 		target: "normal",
 		type: "Flying"
 	},
+	stealthrock: {
+        	inherit: true,
+        	effect: {
+            // this is a side condition
+            	onStart: function(side) {
+                	this.add('-sidestart',side,'move: Stealth Rock');
+            	},
+            	onSwitchIn: function(pokemon) {
+                	var typeMod = this.getEffectiveness('Rock', pokemon);
+                	var factor = 8;
+                	if (typeMod == 1) factor = 6;
+                	if (typeMod >= 2) factor = 4;
+                	if (typeMod == -1) factor = 16;
+                	if (typeMod <= -2) factor = 12;
+				//if (typeMod == 1) factor = 4;
+				//if (typeMod >= 2) factor = 2;
+				//if (typeMod == -1) factor = 16;
+				//if (typeMod <= -2) factor = 32;
+                var damage = this.damage(pokemon.maxhp/factor);
+            	}
+        }
+    	},
 	"technoblast": {
 		inherit: true,
 		basePower: 95,
