84765060641c1950b113ea8f12c619ed7716a35b
King Smogoon <iSmogoon2@gmail.com>
2015-08-11 00:19:58 -0600

    o3o

--- mods/goonmons/rulesets.js
+++ mods/goonmons/rulesets.js
@@ -1,12 +1,12 @@
 exports.BattleFormats = {
-	unrelgens: {
-		effectType: 'Banlist',
+	goondex: {
+		effectType: 'Rule',
 		validateSet: function (set) {
-			var template = this.getTemplate(set.species || set.name);
-			if (template.num < 493) {
-				return [set.species + " is not in this version of GoonMons."];
-			} if (template.num > 649) {
-				return [set.species + " is not in this version of GoonMons."];
+			var unovaDex = {
+				"Serperior":1, "Emboar":1, "Samurott":1, 
+			};
+			if (!(set.species in hoennDex)) {
+				return [set.species + " is not in the Hoenn Pokedex."];
 			}
 		}
 	},
