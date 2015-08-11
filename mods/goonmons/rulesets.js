fa08d872cc92377e04bae2f5efc51f941afa5b5d
King Smogoon <iSmogoon2@gmail.com>
2015-08-11 00:20:36 -0600

    Update rulesets.js

--- mods/goonmons/rulesets.js
+++ mods/goonmons/rulesets.js
@@ -5,7 +5,7 @@ exports.BattleFormats = {
 			var unovaDex = {
 				"Serperior":1, "Emboar":1, "Samurott":1, 
 			};
-			if (!(set.species in hoennDex)) {
+			if (!(set.species in unovaDex)) {
 				return [set.species + " is not in the Hoenn Pokedex."];
 			}
 		}
