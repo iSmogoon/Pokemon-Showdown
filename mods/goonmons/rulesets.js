d2873fb1dffa714e7347382af972b5701bb65065
King Smogoon <iSmogoon2@gmail.com>
2015-08-11 00:51:37 -0600

    Update rulesets.js

--- mods/goonmons/rulesets.js
+++ mods/goonmons/rulesets.js
@@ -3,7 +3,7 @@ exports.BattleFormats = {
 		effectType: 'Rule',
 		validateSet: function (set) {
 			var unovaDex = {
-				"Serperior":1, "Emboar":1, "Samurott":1, 
+				"Serperior":1, "Emboar":1, "Samurott":1, "Watchog":1, "Stoutland":1, "Liepard":1
 			};
 			if (!(set.species in unovaDex)) {
 				return [set.species + " is not in this version of GoonMons."];
